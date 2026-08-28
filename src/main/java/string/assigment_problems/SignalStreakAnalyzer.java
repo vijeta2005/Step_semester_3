3d87a028de4a042bfb5cb5f32e84034bcf021772
vijeta2005 <vs2991@srmist.edu.in>
2026-08-28 23:47:39 +0530

    Complete SignalStreakAnalyzer solution

---
+++ src/main/java/string/assigment_problems/SignalStreakAnalyzer.java
@@ -0,0 +1,37 @@
+package string.assigment_problems;
+public class SignalStreakAnalyzer {
+
+    public static void findLongestStreak(String signalLog) {
+        if (signalLog == null || signalLog.isEmpty()) {
+            System.out.println("No logs available.");
+            return;
+        }
+
+        char maxChar = signalLog.charAt(0);
+        int maxStreak = 1;
+
+        char currentChar = signalLog.charAt(0);
+        int currentStreak = 1;
+
+        for (int i = 1; i < signalLog.length(); i++) {
+            if (signalLog.charAt(i) == currentChar) {
+                currentStreak++;
+            } else {
+                currentChar = signalLog.charAt(i);
+                currentStreak = 1;
+            }
+
+            if (currentStreak > maxStreak) {
+                maxStreak = currentStreak;
+                maxChar = currentChar;
+            }
+        }
+
+        System.out.println("Longest Streak: '" + maxChar + "' repeated " + maxStreak + " times");
+    }
+
+    public static void main(String[] args) {
+        findLongestStreak("RRGGGYRR");
+        findLongestStreak("RRRRYYGG");
+    }
+}
